76903ea82c890b250f4fbe5598505ac25ff6f707
Girjendra <shivaji12889@gmail.com>
2026-05-31 08:40:46 +0530

    Create portfolio application layout

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Journey from "./components/Journey";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Journey />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
